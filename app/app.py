59cfe56cdd42c95469a346ab74e90ec5b9cdec43
Manish <manish13062004@gmail.com>
2026-04-02 14:49:28 +0530

    Trigger pipeline

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Advanced CI/CD on AWS 🚀"
+    return "CI/CD Pipeline Working Successfully ✅"
 
 @app.route("/health")
 def health():
